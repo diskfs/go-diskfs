ffbfc80b269182ca89ef987264ae4aa158db16ad
José Moreira <josemoreiravarzim@gmail.com>
2021-06-18 14:41:28 +0100

    chore: add darwin disk read support
    clean: added requested changes and cleaned the files
    
    chore: changes requested on #125

--- diskfs_darwin.go
+++ diskfs_darwin.go
@@ -1,5 +1,3 @@
-// +build !darwin linux solaris aix freebsd illumos netbsd openbsd plan9
-
 package diskfs
 
 import (
@@ -9,18 +7,26 @@ import (
 	"golang.org/x/sys/unix"
 )
 
+// this constants should be part of "golang.org/x/sys/unix", but aren't, yet
+const (
+	DKIOCGETBLOCKSIZE         = 0x40046418
+	DKIOCGETPHYSICALBLOCKSIZE = 0x4004644D
+	DKIOCGETBLOCKCOUNT        = 0x40086419
+)
+
 // getSectorSizes get the logical and physical sector sizes for a block device
 func getSectorSizes(f *os.File) (int64, int64, error) {
 	/*
 		ioctl(fd, BLKPBSZGET, &physicalsectsize);
+
 	*/
 	fd := f.Fd()
 
-	logicalSectorSize, err := unix.IoctlGetInt(int(fd), unix.BLKSSZGET)
+	logicalSectorSize, err := unix.IoctlGetInt(int(fd), DKIOCGETBLOCKSIZE)
 	if err != nil {
 		return 0, 0, fmt.Errorf("Unable to get device logical sector size: %v", err)
 	}
-	physicalSectorSize, err := unix.IoctlGetInt(int(fd), unix.BLKPBSZGET)
+	physicalSectorSize, err := unix.IoctlGetInt(int(fd), DKIOCGETPHYSICALBLOCKSIZE)
 	if err != nil {
 		return 0, 0, fmt.Errorf("Unable to get device physical sector size: %v", err)
 	}
