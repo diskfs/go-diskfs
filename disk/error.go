af2e622a7b02c93ad651a91a5eec72a33ae9bd73
Avi Deitcher <deitch@users.noreply.github.com>
2025-12-24 10:07:15 +0200

    return specific error types from GetFilesystem (#321)
    Signed-off-by: Avi Deitcher <avi@deitcher.net>

---
+++ disk/error.go
@@ -0,0 +1,39 @@
+package disk
+
+import "fmt"
+
+type UnknownFilesystemError struct {
+	partition int
+}
+
+func (e *UnknownFilesystemError) Error() string {
+	return fmt.Sprintf("unknown filesystem type on partition %d", e.partition)
+}
+
+func NewUnknownFilesystemError(partition int) *UnknownFilesystemError {
+	return &UnknownFilesystemError{
+		partition: partition,
+	}
+}
+
+type NoPartitionTableError struct{}
+
+func (e *NoPartitionTableError) Error() string {
+	return "no partition table found on disk"
+}
+
+type MaxPartitionsExceededError struct {
+	requested int
+	max       int
+}
+
+func (e *MaxPartitionsExceededError) Error() string {
+	return fmt.Sprintf("requested partition %d exceeds maximum partitions %d", e.requested, e.max)
+}
+
+func NewMaxPartitionsExceededError(requested, maxPart int) *MaxPartitionsExceededError {
+	return &MaxPartitionsExceededError{
+		requested: requested,
+		max:       maxPart,
+	}
+}
